ea7c3e0e4f5f4bf165ab43bf7d19cc6d96abe783
melilei <melilei6784@gmail.com>
2024-09-22 16:14:37 +0300

    completed assingment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,17 @@
 def write_file(file_name, file_content):
-    pass
+    file_name = str(file_name)  # Convert file_name to string
+    with open(file_name + ".txt", "w") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    file_name = str(file_name)  # Convert file_name to string
+    with open(file_name + ".txt", "a") as file:
+        if file.tell() > 0:  # Check if file is not empty
+            file.write("\n")  # Add newline only if file is not empty
+        file.write(append_content)  # Append content
 
 def read_file(file_name):
-    pass
+    file_name = str(file_name)  # Convert file_name to string
+    with open(file_name + ".txt", "r") as file:
+        content = file.read()
+    return content
